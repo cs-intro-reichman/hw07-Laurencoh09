a76994d05fd755a1479573aeb99d2c2f8b9384eb
Lauren Cohen <154359581+Laurencoh09@users.noreply.github.com>
2024-02-08 00:36:41 +0200

    Update SpellChecker.java

--- SpellChecker.java
+++ SpellChecker.java
@@ -11,25 +11,55 @@ public class SpellChecker {
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		str = str.substring(1);
+		return str;
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
-	}
+		 word1 = word1.toLowerCase();
+		 word2 = word2.toLowerCase();
+
+		 if (word1.charAt(0)== word2.charAt(0)){
+           return levenshtein (tail(word1), tail(word2));
+	     }
+
+        else if( word1.isEmpty() || word2.isEmpty()){
+           return (Math.max (word1.lenght(),word2.lenght()));
+        
+        } 
+
+	     else {
+
+			return 1 + (Math.min(levenshtein(tail(word1), tail(word2)),Math.min(levenshtein((word1), tail(word2)),levenshtein(tail(word1), (word2))))); 
+			}
+		}	
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
 
 		In in = new In(fileName);
 
-		// Your code here
+		for(int j = 0; j < 3000; j++){
+		dictionary[j] = in.readString();
+
+	    }  
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		String correct = word;
+			int currentTresh=0;
+			int smallest = threshold+1;
+		for(int j = 0; j < dictionary.length; j++) {
+			currentTresh = levenshtein(dictionary[j], word);
+			if(currentTresh <= threshold && currentTresh != smallest )
+			{
+				correct = dictionary[j];
+				smallest = currentTresh;
+			}
+		}
+		return correct;
 	}
 
 }
